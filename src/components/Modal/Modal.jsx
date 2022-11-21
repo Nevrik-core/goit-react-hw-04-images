27f502e3b632746c478c40232a2862512aea0e59
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-11-21 20:37:37 +0100

    fixed mentor remarks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,36 +3,40 @@ import PropTypes from 'prop-types';
 
 import { Overlay, ModalEl, Image } from "./Modal.styled";
 
-export default function Modal({largeImage, toggleModal}) {
+export default function Modal({ largeImage, toggleModal }) {
 
   useEffect(() => {
+
+
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        toggleModal()
+      }
+    };
+
     window.addEventListener('keydown', handleKeyDown);
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     }
-  })
+
+  }, [toggleModal]);
 
 
   const handleBackdropClick = e => {
     if (e.target === e.currentTarget) {
-       toggleModal()
-     }
-  };
-
-  const handleKeyDown = e => {
-    if (e.code === 'Escape') {
       toggleModal()
     }
   };
 
-    return (
-      <Overlay  onClick={handleBackdropClick}>
-        <ModalEl >
-          <Image src={largeImage} alt=""></Image>
-        </ModalEl>
-      </Overlay>
-    );
-  
+
+  return (
+    <Overlay onClick={handleBackdropClick}>
+      <ModalEl >
+        <Image src={largeImage} alt=""></Image>
+      </ModalEl>
+    </Overlay>
+  );
+
 }
 
 Modal.propTypes = {
